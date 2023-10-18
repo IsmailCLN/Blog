379fa2bb165d4733a3a1877be60cdf1f7d8307e1
İsmail ÇELEN <81293395+IsmailCLN@users.noreply.github.com>
2023-10-18 19:28:32 +0300

    views added

--- posts/views.py
+++ posts/views.py
@@ -4,3 +4,6 @@ from .models import Post
 def index(request):
     myObject = Post.objects.all().first()
     return render(request,'posts/index.html',{'post':myObject})
+
+def single(request, post_id):
+    return render(rquest,'posts/single.html', {'single':post_id})
