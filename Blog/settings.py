86a917aed5fca49348419f4295ece6ffad012c28
İsmail ÇELEN <81293395+IsmailCLN@users.noreply.github.com>
2023-11-07 15:05:14 +0300

    Template Completed

--- Blog/settings.py
+++ Blog/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -110,3 +111,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+   # '/var/www/static/',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT =  os.path.join(BASE_DIR, "media_cdn")
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,37 @@
+"""Blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path
 from . import views
+from django.urls import path
+from .views import CreatePostView,IndexView,BlogView,PostDetail,PostDeleteView,UpdatePostView
+
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('detail/<int:post_id>', views.single, name="single"),
+    path('', IndexView.as_view(), name='index'),
+    path('detail/<slug:slug>', views.PostDetail.as_view(),name='single'),
+    path('detail/<slug:slug>/delete', views.PostDeleteView.as_view(),name='delete'),
+    path('detail/<slug:slug>/update', views.UpdatePostView.as_view(),name='update'),
+    path('create/', CreatePostView.as_view(), name="createPost"),
+    path('blog/', BlogView.as_view(), name="blog"),
+
+    #category
+    path('category/<slug:slug>', views.CategoryDetail.as_view(),name='category-detail'),
+    path('tag/<slug:slug>', views.TagDetail.as_view(),name='tag-detail'),
+    path('search/',views.SearchView.as_view(),name="search"),
+
+    path('comment/<int:pk>/delete',views.CommentDeleteView.as_view(),name="comment-delete")
+
 ]
